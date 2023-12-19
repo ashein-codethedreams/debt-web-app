a13c74bd327f0c6033f9600b8c162e2a5dc7fc83
unknown <heroglasses155@gmail.com>
2023-12-19 13:44:37 +0630

    get firebase db data

--- src/features/List/AllList.jsx
+++ src/features/List/AllList.jsx
@@ -1,22 +1,6 @@
 import { Table } from "antd";
 import MainLayout from "../../components/UI/Layout";
-
-const dataSource = [
-  {
-    key: "1",
-    name: "Ashein",
-    title: "ဝက်သား",
-    amount: 3000,
-    date: "10/12/2023",
-  },
-  {
-    key: "2",
-    name: "Zon",
-    title: "ကြိုး",
-    amount: 10000,
-    date: "24/10/2023",
-  },
-];
+import { useEffect, useState } from "react";
 
 const columns = [
   {
@@ -48,15 +32,54 @@ const columns = [
 ];
 
 const AllList = () => {
+  const [lists, setLists] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchLists = async () => {
+      const response = await fetch(
+        "https://expense-app-562ec-default-rtdb.firebaseio.com/lists.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!!!");
+      }
+
+      const responseData = await response.json();
+      const loadedLists = [];
+      for (const key in responseData) {
+        loadedLists.push({
+          key: key,
+          id: key,
+          title: responseData[key].item_name,
+          name: responseData[key].payer_name,
+          date: responseData[key].date,
+          amount: responseData[key].amount,
+        });
+      }
+
+      setLists(loadedLists);
+      setIsLoading(false);
+    };
+
+    fetchLists().catch((error) => {
+      setIsLoading(false);
+      console.log(error.message);
+    });
+  }, []);
+
   return (
     <div>
       <MainLayout>
         <p className="text-lg font-semibold">All History</p>
-        <Table
-          style={{ margin: "30px" }}
-          dataSource={dataSource}
-          columns={columns}
-        />
+        {isLoading ? (
+          <p className="p-5">Loading...</p>
+        ) : (
+          <Table
+            style={{ margin: "30px" }}
+            dataSource={lists}
+            columns={columns}
+          />
+        )}
       </MainLayout>
     </div>
   );
